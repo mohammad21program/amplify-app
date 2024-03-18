e18d9a5ad9d76af494dc4b5a3ba2dd8d396edbb8
Rezaul Karim <mohammad21program@gmail.com>
2024-03-18 14:26:06 +0600

    Deloy description added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from v2</h1>
+        <h3>Amplify Deployed on AWS</h3>
+        <a href='https://aws.amazon.com/'>Connect to AWS</a>
       </header>
     </div>
   );
