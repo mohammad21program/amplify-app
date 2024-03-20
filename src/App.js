6cd5e929cd7048f09bd4543787302783b410ab92
Rezaul Karim <mohammad21program@gmail.com>
2024-03-20 15:12:21 +0600

    Added v2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from "./logo.svg";
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
   );
 }
 
